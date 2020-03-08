a24f15651cf92ad9f4d71e06517674b4169f0b3e
Anthony Shuker <anthony@emobe.uk>
2020-03-08 15:36:02 +0000

    eh

---
+++ src/components/Item.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="name item">
+    <h2>{{ title }}</h2>
+    <ul>
+      <li v-for="item in section" :key="item[1]">
+        <a v-if="item[1]" :href="item[1]">
+          {{ item[0] }}
+        </a>
+        <span v-else>
+          {{ item[0] }}
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Item extends Vue {
+  @Prop(Array) readonly section!: string[];
+  @Prop(String) readonly title!: string;
+}
+</script>
+<style lang="scss" scoped>
+$padding: 27px;
+h2 {
+  font-size: 9vw;
+  color: grey;
+  padding-left: $padding;
+  margin-bottom: 10px;
+}
+a {
+  color: whitesmoke;
+  &:hover {
+    color: grey;
+  }
+}
+.item {
+  font-size: 10vw;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: whitesmoke;
+  margin-bottom: 50px;
+  ul {
+    list-style-type: none;
+    margin-top: 0;
+    padding-left: $padding;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <item
+      v-for="(section, key) in sections"
+      :section="section"
+      :title="key"
+      :key="key"
+      :id="key"
+    />
+    <div class="side">
+      <a href="#about">About</a>
+      <a href="#skills">Skills</a>
+      <a href="#portfolio">Portfolio</a>
+      <a href="#contact">Contact</a>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Item from "@/components/Item";
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: { Item },
+  data() {
+    return {
+      sections: {
+        about: [["Leeds"], ["Based"], ["Web"], ["Developer"], ["10+ years"]],
+        skills: [
+          ["HTML"],
+          ["Javascript"],
+          ["Typescript"],
+          ["vue"],
+          ["react"],
+          ["node"]
+        ],
+        portfolio: [
+          ["Github", "https://github.com/emobe"],
+          ["npm", "https://npmjs.com/~emobe"],
+          ["LendTech", "https://crunchbase.com/organization/zikher-inc"]
+        ],
+        contact: [
+          ["Email", "mailto:anthony@emobe.uk"],
+          ["LinkedIn", "https://www.linkedin.com/in/anthony-shuker"]
+        ]
+      }
+    };
   }
 };
 </script>
+<style lang="scss" scoped>
+.home {
+  background-color: black;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.side {
+  a {
+    color: black;
+    margin: 10px 0;
+    font-size: 4vw;
+  }
+  position: fixed;
+  width: 25vw;
+  height: 100vh;
+  background-color: white;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  font-weight: bold;
+  padding: 15px;
+  box-sizing: border-box;
+}
+</style>
